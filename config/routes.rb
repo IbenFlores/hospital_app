fe55dde716fd116fe693fab1a4403108275b1881
iben polanco <iben.fpolanco@gmail.com>
2025-05-25 18:37:11 -0500

    visualizacion html devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "pages#home"
   devise_for :pacientes
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
